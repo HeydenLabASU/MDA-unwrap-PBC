053b85d518cdf68804a4d77007486817606dc885
Matthias Heyden <mheyden1@asu.edu>
2024-07-12 10:57:15 -0700

    moved loop break

--- MDA_unwrap_PBC/ctypes_lib/unwrap.c
+++ MDA_unwrap_PBC/ctypes_lib/unwrap.c
@@ -42,11 +42,7 @@ int buildTree(    t_branch *tree,
     while(i<nBonds && count<10) {
         b0=bondList[i*2+0];
         b1=bondList[i*2+1];
-        if(b0>node && b1>node) {
-            /*bondList is assumed to be sorted*/
-            /*therefore we can break the loop now*/
-            break;
-        }
+    
         if((b0<0 || b0>=nAtoms) || (b1<0 || b1>=nAtoms)) {
             FILE *err;
             err=fopen("error.log","a");
@@ -71,6 +67,11 @@ int buildTree(    t_branch *tree,
             bondStart[0]=i;
         }
         i++;
+        if(b0>node && b1>node) {
+            /*bondList is assumed to be sorted*/
+            /*therefore we can break the loop now*/
+            break;
+        }
     }
     if(count==10) {
         FILE *err;
