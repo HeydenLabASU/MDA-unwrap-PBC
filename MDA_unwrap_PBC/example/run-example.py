095ec4abb4213775f1989810a60d4ac437f2b786
Matthias Heyden <mheyden1@asu.edu>
2024-09-19 12:24:47 -0700

    improved user interface
    - working directory on trajectory timestep instead of passing it as an array
    - renamed method "unwrap" to "run"

--- MDA_unwrap_PBC/example/run-example.py
+++ MDA_unwrap_PBC/example/run-example.py
@@ -3,7 +3,7 @@ Usage example for mda_unwrap_pbc package
 """
 
 import MDAnalysis as mda
-import MDA_unwrap_PBC as unwrap
+import MDA_unwrap_PBC as pbc
 from MDA_unwrap_PBC.example.datafiles import *
 
 #input parameters
@@ -11,10 +11,10 @@ from MDA_unwrap_PBC.example.datafiles import *
 u = mda.Universe(TOPOL,TRAJ)
 nRead=len(u.trajectory)
 
-trees = unwrap.unwrap.buildTrees(u)
+trees = pbc.unwrap.buildTrees(u)
 
 #loop over trajectory, apply unwrap, and write out coordinates
 with mda.Writer(TRAJ_PBC, len(u.atoms)) as w:
     for ts in u.trajectory:
-        u.trajectory.ts._pos=unwrap.unwrap.unwrap(u,trees)
+        pbc.unwrap.run(u,trees)
         w.write(u)
